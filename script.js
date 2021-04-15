45d5703e5f83df4e8adeae8e56b6746368ab09eb
ScriptKid4you <59839847+ScriptKid4you@users.noreply.github.com>
2021-04-15 18:38:04 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const countBtn = document.querySelector(".countBtnDiv button");
+const resetBtn = document.querySelector(".resetBtnDiv button");
+let countNum = document.querySelector(".countNum p");
+
+countBtn.addEventListener("click", countUp);
+resetBtn.addEventListener("click", reset);
+
+function countUp() {
+  countNum.innerHTML++;
+}
+
+function reset(){
+  countNum.innerHTML= 0;
+}
