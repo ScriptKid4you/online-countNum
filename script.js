a65ff26ec103a254958c85042c0154108d3a7ec3
ScriptKid4you <59839847+ScriptKid4you@users.noreply.github.com>
2021-04-15 23:02:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const countBtn = document.querySelector(".countBtnDiv button");
 const resetBtn = document.querySelector(".resetBtnDiv button");
 let countNum = document.querySelector(".countNum p");
@@ -7,8 +9,26 @@ resetBtn.addEventListener("click", reset);
 
 function countUp() {
   countNum.innerHTML++;
+
 }
 
 function reset(){
   countNum.innerHTML= 0;
 }
+
+const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
